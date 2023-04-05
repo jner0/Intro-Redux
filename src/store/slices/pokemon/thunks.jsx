5a3bd2d916808b7869e9c04e6fc9b5a69c797f06
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-04-04 21:46:25 -0500

    Axios

--- src/store/slices/pokemon/thunks.jsx
+++ src/store/slices/pokemon/thunks.jsx
@@ -1,3 +1,4 @@
+import { pokemonApi } from "../../../api/pokemonApi";
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice";
 
 
@@ -7,8 +8,12 @@ export const getPokemons = ( page = 0 ) => {
         dispatch( startLoadingPokemons() );
 
         //TODO: realizar peticion http
+        // const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${ page * 10 }`);
+        // const data = await resp.json();
+        const {} = await pokemonApi.get(`/pokemon?limit=10&offset=${ page * 10 }`);
+        //console.log(data);
 
-        // dispatch( setPokemons() );
+        dispatch( setPokemons({ pokemons: data.results, page: page + 1 }) );
     }
 
 }
